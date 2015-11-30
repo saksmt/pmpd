cec334a142195f521c409f7e49f7c6f0b0824e5b
smt <kirillsaksin@yandex.ru>
2015-11-30 13:30:35 +0300

    Fixed #1. Now authentication works

---
+++ src/Smt/Pmpd/Exception/InvalidPasswordException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Smt\Pmpd\Exception;
+
+/**
+ * Thrown on invalid password provided
+ * @package Smt\Pmpd\Exception
+ * @author Kirill Saksin <kirillsaksin@yandex.ru>
+ */
+class InvalidPasswordException extends \Exception
+{
+}
